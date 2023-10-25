03e1d646ea7cc1bb0581e38235d57e99b5432b0b
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-10-25 17:54:28 +0530

    k

--- trivy-image-scan.sh
+++ trivy-image-scan.sh
@@ -1,22 +1,22 @@
-# #!/bin/bash
+#!/bin/bash
 
-# dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
-# echo $dockerImageName
+dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
+echo $dockerImageName
 
-# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-#     # Trivy scan result processing
-#     exit_code=$?
-#     echo "Exit Code : $exit_code"
+    # Trivy scan result processing
+    exit_code=$?
+    echo "Exit Code : $exit_code"
 
-#     # Check scan results
-#     if [[ "${exit_code}" == 1 ]]; then
-#         echo "Image scanning failed. Vulnerabilities found"
-#         exit 1;
-#     else
-#         echo "Image scanning passed. No CRITICAL vulnerabilities found"
-#     fi;
+    # Check scan results
+    if [[ "${exit_code}" == 1 ]]; then
+        echo "Image scanning failed. Vulnerabilities found"
+        exit 1;
+    else
+        echo "Image scanning passed. No CRITICAL vulnerabilities found"
+    fi;
 
 #!/bin/bash
 
@@ -34,24 +34,24 @@ if ! docker info > /dev/null 2>&1; then
     exit 1
 fi
 
-# Define the Docker image name
-dockerImageName="dsocouncil/node-service:b9b977a211d98b6389b8ea752c921c6729215adc"  # Replace with the correct image name and tag
+# # Define the Docker image name
+# dockerImageName="dsocouncil/node-service:b9b977a211d98b6389b8ea752c921c6729215adc"  # Replace with the correct image name and tag
 
-# Pull the Docker image (if not already available locally)
-docker pull $dockerImageName
+# # Pull the Docker image (if not already available locally)
+# docker pull $dockerImageName
 
-# Run Trivy to scan the Docker image using Docker explicitly
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light --input $dockerImageName
+# # Run Trivy to scan the Docker image using Docker explicitly
+# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light --input $dockerImageName
 
-# Trivy scan result processing
-exit_code=$?
-echo "Exit Code : $exit_code"
+# # Trivy scan result processing
+# exit_code=$?
+# echo "Exit Code : $exit_code"
 
-# Check scan results
-if [[ "${exit_code}" == 1 ]]; then
-    echo "Image scanning failed. Vulnerabilities found"
-    exit 1
-else
-    echo "Image scanning passed. No CRITICAL vulnerabilities found"
-fi
+# # Check scan results
+# if [[ "${exit_code}" == 1 ]]; then
+#     echo "Image scanning failed. Vulnerabilities found"
+#     exit 1
+# else
+#     echo "Image scanning passed. No CRITICAL vulnerabilities found"
+# fi
 
