70ecf9a150c791a1412695631be22165dcb02bf4
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-10-25 17:21:51 +0530

    k
    k

--- trivy-image-scan.sh
+++ trivy-image-scan.sh
@@ -1,19 +1,39 @@
-#!/bin/bash
+# #!/bin/bash
 
-dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
-echo $dockerImageName
+# dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
+# echo $dockerImageName
 
+# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+
+#     # Trivy scan result processing
+#     exit_code=$?
+#     echo "Exit Code : $exit_code"
+
+#     # Check scan results
+#     if [[ "${exit_code}" == 1 ]]; then
+#         echo "Image scanning failed. Vulnerabilities found"
+#         exit 1;
+#     else
+#         echo "Image scanning passed. No CRITICAL vulnerabilities found"
+#     fi;
+
+#     #!/bin/bash
+
+# Define the Docker image name
+dockerImageName="dsocouncil/node-service"
+
+# Run Trivy to scan the Docker image
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-    # Trivy scan result processing
-    exit_code=$?
-    echo "Exit Code : $exit_code"
+# Trivy scan result processing
+exit_code=$?
+echo "Exit Code : $exit_code"
 
-    # Check scan results
-    if [[ "${exit_code}" == 1 ]]; then
-        echo "Image scanning failed. Vulnerabilities found"
-        exit 1;
-    else
-        echo "Image scanning passed. No CRITICAL vulnerabilities found"
-    fi;
+# Check scan results
+if [[ "${exit_code}" == 1 ]]; then
+    echo "Image scanning failed. Vulnerabilities found"
+    exit 1
+else
+    echo "Image scanning passed. No CRITICAL vulnerabilities found"
+fi
