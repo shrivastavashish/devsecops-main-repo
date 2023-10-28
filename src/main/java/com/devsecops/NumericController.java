9d42b7468980cad8ef32db357bce899d68dbd3f3
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-10-28 19:43:26 +0530

    k

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -2,7 +2,7 @@ package com.devsecops;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
