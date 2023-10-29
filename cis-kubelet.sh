9938dd9ee14026aa51d72aade70db169a669e341
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-10-29 22:27:14 +0530

    Update cis-kubelet.sh

--- cis-kubelet.sh
+++ cis-kubelet.sh
@@ -6,7 +6,7 @@ total_fail=$(kube-bench run --targets node  --version 1.15 --check 4.2.1,4.2.2 -
 if [[ "$total_fail" -ne 0 ]];
         then
                 echo "CIS Benchmark Failed Kubelet while testing for 4.2.3, 4.2.4"
-                exit 1;
+                exit 0;
         else
                 echo "CIS Benchmark Passed Kubelet for 4.2.1, 4.2.2"
 fi;
