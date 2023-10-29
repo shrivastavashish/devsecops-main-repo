a6411e78eeea9f4a1040e6285ca0be38062b1b06
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-10-29 19:55:44 +0530

    base image updated

--- trivy-scan.sh
+++ trivy-scan.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 
-dockerImageName=openjdk:8-jdk-alpine
+dockerImageName=openjdk:22
 echo $dockerImageName
 
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
