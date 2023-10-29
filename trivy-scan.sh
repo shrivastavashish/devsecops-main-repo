49296fb94ae7217366f3eedd03baa26fb9bdf85e
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-10-29 11:16:41 +0530

    Update trivy-scan.sh

--- trivy-scan.sh
+++ trivy-scan.sh
@@ -1,19 +1,19 @@
-# !/bin/bash
+!/bin/bash
 
-# dockerImageName=openjdk:22
-# echo $dockerImageName
+dockerImageName=openjdk:22
+echo $dockerImageName
 
-# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-# docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
-#     # Trivy scan result processing
-#     exit_code=$?
-#     echo "Exit Code : $exit_code"
+    # Trivy scan result processing
+    exit_code=$?
+    echo "Exit Code : $exit_code"
 
-#     # Check scan results
-#     if [[ "${exit_code}" == 1 ]]; then
-#         echo "Image scanning failed. Vulnerabilities found"
-#         exit 1;
-#     else
-#         echo "Image scanning passed. No CRITICAL vulnerabilities found"
+    # Check scan results
+    if [[ "${exit_code}" == 1 ]]; then
+        echo "Image scanning failed. Vulnerabilities found"
+        exit 1;
+    else
+        echo "Image scanning passed. No CRITICAL vulnerabilities found"
     fi;
