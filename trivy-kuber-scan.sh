887874eec49134e66e48b13f63ee87eeaa8e4d2f
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-10-29 12:02:01 +0530

    Update trivy-kuber-scan.sh

--- trivy-kuber-scan.sh
+++ trivy-kuber-scan.sh
@@ -4,7 +4,7 @@
 #echo $imageName #getting Image name from env variable
 ImageName=openjdk:22
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 0 --severity HIGH --light $imageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
     # Trivy scan result processing
