5596a0e047b67379b2499de156e8a07788a9a037
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-11-04 01:10:23 +0530

    Update trivy-scan.sh

--- trivy-scan.sh
+++ trivy-scan.sh
@@ -13,7 +13,7 @@ docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.46.0 -q image --exit
     # Check scan results
     if [[ "${exit_code}" == 1 ]]; then
         echo "Image scanning failed. Vulnerabilities found"
-        exit 0;
+        exit 1;
     else
         echo "Image scanning passed. No CRITICAL vulnerabilities found"
     fi;
