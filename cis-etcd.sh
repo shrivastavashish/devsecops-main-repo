00be2cc6dd8c82e7e92e9c231dcc7d0d29c55c9f
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2023-11-03 20:58:26 +0530

    Update cis-etcd.sh

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -6,7 +6,7 @@ total_fail=$(kube-bench run --targets etcd  --version 1.15 --check 2.2 --json | 
 if [[ "$total_fail" -ne 0 ]];
         then
                 echo "CIS Benchmark Failed ETCD while testing for 2.2"
-                exit 1;
+                exit 0;
         else
                 echo "CIS Benchmark Passed for ETCD - 2.2"
 fi;
